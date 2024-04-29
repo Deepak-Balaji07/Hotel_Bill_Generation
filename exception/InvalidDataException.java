049b67d7920f7acdb7f0be79ccc6c1395a4e2f58
Deepak-Balaji07 <147257487+Deepak-Balaji07@users.noreply.github.com>
2024-04-29 22:37:33 +0530

    Add files via upload

---
+++ exception/InvalidDataException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class InvalidDataException {
+	 @Override
+	    public String toString() {
+	        return "Invalid Data";
+	    }
+}
---
+++ exception/InvalidRoomTypeException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class InvalidRoomTypeException {
+	@Override
+    public String toString() {
+        return "Invalid Room Type";
+    }
+
+}
